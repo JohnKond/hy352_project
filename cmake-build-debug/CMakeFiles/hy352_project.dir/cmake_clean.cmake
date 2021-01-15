efeb7266812fbf743bf8714dab312bb7120bda16
kondo <gkontogiorgakes@gmail.com>
2021-01-15 23:40:47 +0200

    additional stuff

---
+++ cmake-build-debug/CMakeFiles/hy352_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hy352_project.dir/main.cpp.o"
+  "hy352_project"
+  "hy352_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hy352_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
